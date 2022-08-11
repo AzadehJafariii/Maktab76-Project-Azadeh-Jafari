81aeb3223a84955ca180e62e36a02a72d450892c
AzadehJafariii <jafari.azadeh2018@gmail.com>
2022-08-11 21:43:03 +0430

    ProjectStructure

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import AppRoutes from "./routes/app.routes";
+
+const theme = createTheme({
+  direction: "rtl",
+});
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+function App(props) {
+  return (
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <AppRoutes />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default App;
